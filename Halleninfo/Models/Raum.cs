ff76149cf2c447e9870451ab652b8663b1ed5368
Janis Schenk <janis.schenk@gmail.com>
2025-11-19 17:31:28 +0100

    Pages

--- Halleninfo/Models/Raum.cs
+++ Halleninfo/Models/Raum.cs
@@ -1,7 +1,31 @@
-
+using System.Reflection;
 using xTend;
+
 namespace Halleninfo {
     public partial class Raum {
-        public override Permission Permission => Permission.Read;
+        public override Permission Permission {
+            get {
+                if (Database.CurrentUser.IsAdmin()) return Permission.All;
+                if (Database.CurrentUser.Usergroups.Contains(xtUsergroup)) return Permission.All;
+                if (Database.CurrentUser.Usergroups.Any()) return Permission.Read | Permission.Change | Permission.New;
+                return Permission.None;
+            }
+        }
+
+        public override string? AdditionalSQLFilter {
+            get {
+                if (Database.CurrentUser.IsAdmin()) return null;
+                return $"{nameof(Raum)}.{nameof(xtUsergroup_FK)} = {Database.CurrentUser.Usergroups.First().xtUsergroup_ID}";
+            }
+        }
+
+        public override void FilterForeignData(PropertyInfo prop, List<DBO> foreignDBOs) {
+            base.FilterForeignData(prop, foreignDBOs);
+
+            if (!Database.CurrentUser.IsAdmin() && prop.Name == nameof(xtUsergroup_FK)) {
+                foreignDBOs.Clear();
+                foreignDBOs.AddRange(Database.CurrentUser.Usergroups);
+            }
+        }
     }
 }
--- Halleninfo/Models/State.cs
+++ Halleninfo/Models/State.cs
@@ -1,7 +1,44 @@
-
+using System.Reflection;
 using xTend;
+
 namespace Halleninfo {
     public partial class State {
-        public override Permission Permission => Permission.Read;
+        public override Permission Permission {
+            get {
+                if (Database.CurrentUser.IsAdmin()) return Permission.All;
+                if (Database.CurrentUser.Usergroups.Contains(xtUsergroup)) return Permission.Read | Permission.Change | Permission.New;
+                if (Database.CurrentUser.Usergroups.Any()) return Permission.Read | Permission.Change | Permission.New;
+                return Permission.None;
+            }
+        }
+
+        public override string? AdditionalSQLFilter {
+            get {
+                if (Database.CurrentUser.IsAdmin()) return null;
+                return $"{nameof(State)}.{nameof(xtUsergroup_FK)} = {Database.CurrentUser.Usergroups.First().xtUsergroup_ID}";
+            }
+        }
+
+        public override void FilterForeignData(PropertyInfo prop, List<DBO> foreignDBOs) {
+            base.FilterForeignData(prop, foreignDBOs);
+
+            if (!Database.CurrentUser.IsAdmin() && prop.Name == nameof(xtUsergroup_FK)) {
+                foreignDBOs.Clear();
+                foreignDBOs.AddRange(Database.CurrentUser.Usergroups);
+            }
+        }
+        
+        public override List<PropertyInfo> EditableProperties { 
+            get {
+                var list = base.ShowProperties;
+                
+                if (!Database.CurrentUser.IsAdmin()) {
+                    list.RemoveAll(l => l.Name == nameof(xtUsergroup_FK));
+                    list.RemoveAll(l => l.Name == nameof(State_ID));
+                }
+                
+                return list;
+            }
+        }
     }
 }
